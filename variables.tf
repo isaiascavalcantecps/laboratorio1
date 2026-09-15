a29668ede1d6130f5fadde296e54a52752247b35
Isaias Cavalcante <isaiascavalcantecps@gmail.com>
2026-09-15 11:19:50 -0300

    feat: variables.tf created

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  type        = string
+  description = "Networking CIDR Block to be used for VPC"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name to be used for the resources (Name tag)"
+}
--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc" "eks_lab01_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_block
 
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "main"
+    Name = "${var.project_name}-vpc"
   }
 }
