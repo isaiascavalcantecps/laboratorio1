8ec83dae9dd9c31d77978126f0f8ee596a515cda
Isaias Cavalcante <isaiascavalcantecps@gmail.com>
2026-09-24 19:12:39 -0300

    feat: Pipeline CI/CD

--- modules.tf
+++ modules.tf
@@ -4,5 +4,3 @@ module "eks_network" {
   project_name = var.project_name
   tags         = local.tags
 }
-#source é o parametro que informa de onde esta buscando, pode buscar tanto de um rep git, pode usar
-#modulo feito por outra pessoa ou pode buscar um diretório que está local.
